246d8886e97d5aed2aaac5fd6b92c8a4d0a45ae0
Marco Eidinger <marco.eidinger@sap.com>
2020-04-02 20:39:01 -0700

    swift package init

---
+++ Sources/swift-package-os-example/swift_package_os_example.swift
@@ -0,0 +1,3 @@
+struct swift_package_os_example {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_package_os_exampleTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_package_os_exampleTests.allTests()
+XCTMain(tests)
