4873d671688b8b7dd82507c2a46ae8329232cb9e
Marco Eidinger <marco.eidinger@sap.com>
2020-06-05 19:40:05 -0700

    more tests

---
+++ Tests/swift-package-os-exampleTests/AnotherApiTests.swift
@@ -0,0 +1,27 @@
+//
+//  AnotherApiTests.swift
+//  swift-package-os-exampleTests
+//
+//  Created by Eidinger, Marco on 6/5/20.
+//
+
+import XCTest
+@testable import swift_package_os_example
+
+class AnotherApiTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AnotherApi().text, "Hello, World!")
+    }
+
+    func testMeaningOfLife() {
+        XCTAssertEqual(AnotherApi().meaningOfLife, 42)
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+
+}
