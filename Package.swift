6760be023a4237b73e5c2ba2cd50ccf7e7695e49
MarcoEidinger <eidingermarco@gmail.com>
2020-10-10 07:10:16 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/objcio/tiny-networking", .exact("0.4.0"),
-        .package(url: "https://github.com/marmelroy/Zip.git", .exact("2.0.0"))
+        .package(url: "https://github.com/marmelroy/Zip.git", .exact("2.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
