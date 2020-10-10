92168005f82a7d16158c1e2de41a3575b2cd1850
MarcoEidinger <eidingermarco@gmail.com>
2020-10-10 06:51:58 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-package-os-example",
-            dependencies: []),
+            dependencies: ["TinyNetworking", "Zip"]),
         .testTarget(
             name: "swift-package-os-exampleTests",
             dependencies: ["swift-package-os-example"]),
